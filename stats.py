f25e4b13a49f6612ed8e4cd8be6eece65bd9d27d
Tapomay <tapomay.dey@gmail.com>
2016-05-15 14:17:41 -0700

    Added perf tests

--- stats.py
+++ stats.py
@@ -33,20 +33,76 @@ def testLCF():
 
     # self.assertRaises(InternalError, Graph.LCF, 12, (5, -5), -3)
 
+def testVF2(g1, g2):
+    t0 = pc()
+    print("VF2: " + str(g1.isomorphic(g2)))
+    return pc() - t0
+
+def testWL(algo, g1, g2):
+    t0 = pc()
+    isomorphic = algo.execute(g1, g2, 12)
+    print("WL: " + str(isomorphic))
+    return pc() - t0
+
+def testLCFStat12():
+    g1=Graph.LCF(12, (5, -5), 6)
+    g2=Graph.Famous("Franklin")
+    t1 = testVF2(g1, g2)
+    wl = WL()
+    t2 = testWL(wl, g1, g2)
+    return ['testLCFStat12', t1, t2]    
+
+def testFull100ISO():
+    g1 = Graph.Full(100)
+    g2 = Graph.Full(100)
+    t1 = testVF2(g1, g2)
+    wl = WL()
+    t2 = testWL(wl, g1, g2)
+    return ['testFull100ISO', t1, t2]
+
+def testFull1000ISO():
+    g1 = Graph.Full(1000)
+    g2 = Graph.Full(1000)
+    t1 = testVF2(g1, g2)
+    wl = WL()
+    t2 = testWL(wl, g1, g2)
+    return ['testFull1000ISO', t1, t2]
+
+def testFull1000NONISO():
+    return []
+
+def testTREE100ISO():
+    return []
+
+def testTREE1000ISO():
+    return []
+
 
 def main():
     formula = "Alice-Bob-Cecil-Alice, Daniel-Cecil-Eugene, Cecil-Gordon"
     g = Graph.Formula(formula)
     # displayG(g)
-    testLCF()
+    # testLCF()
+    stat1 = testLCFStat12()
+    stat2 = testFull100ISO()
+    stat3 = testFull1000ISO()
+    stat4 = testFull1000NONISO()
+    stat5 = testTREE100ISO()
+    stat6 = testTREE1000ISO()
 
-tracer = trace.Trace(
-    ignoredirs=[sys.prefix, sys.exec_prefix],
-    trace=0,
-    count=1)
+    ret = [stat1, stat2, stat3, stat4, stat5, stat6]
+    return ret
 
-tracer.run('main()')
-r = tracer.results()
+stats  = main()
+print(str(stats))
+
+# tracer = trace.Trace(
+#     ignoredirs=[sys.prefix, sys.exec_prefix],
+#     trace=0,
+#     count=1)
+
+# tracer.run('main()')
+# r = tracer.results()
 # r.write_results(show_missing=True, coverdir=".")
 # print(str(r))
 
